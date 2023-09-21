9b0990a8ac1d6b09983a546029ff23d0814a79a4
dean4q <kyrybonis@gmail.com>
2023-09-21 16:05:58 +0300

    добавлена параметризация теста

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import time
 
+import pytest
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import UnexpectedAlertPresentException
 from .pages.base_page import BasePage
@@ -8,12 +9,19 @@ from .pages.main_page import MainPage
 from .pages.product_page import *
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
+
+
+
+@pytest.mark.parametrize('link', urls)
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_basket()
-    browser.implicitly_wait(3)
     page.solve_quiz_and_get_code()
 
 
+    assert page.find_product_name() == page.check_equal_product_name()
+    # print()
+    # print("Цена книги", page.find_product_price())
